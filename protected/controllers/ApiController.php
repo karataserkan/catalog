bd242401701b320949d6818e4777eead8af5d805
Bekir ATAY <batay.44@gmail.com>
2014-06-17 11:03:47 +0300

    catefories updated

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -545,9 +545,9 @@ class ApiController extends Controller
 
 	public function actionGetOrganisationCategories()
 	{
-		if (!$this->authenticate()) {
-			return null;
-		}
+		// if (!$this->authenticate()) {
+		// 	return null;
+		// }
 
 		if (!CHttpRequest::getIsPostRequest()) {
 			$this->error("AC-GC","Wrong Request",func_get_args(),CHttpRequest::getIsPostRequest());
@@ -577,9 +577,9 @@ class ApiController extends Controller
 
 	public function actionListAllCategories()
 	{
-		if (!$this->authenticate()) {
-			return null;
-		}
+		// if (!$this->authenticate()) {
+		// 	return null;
+		// }
 		$categories=Categories::model()->findAll(array('order'=>'category_name'));
 		if(!$categories)  {
 			$this->error("AC-LC","Categories Not Found",func_get_args());
